252088c4e0ce069f3ae659bc2d3afb2a43bfa225
Luis-omega <73986926+Luis-omega@users.noreply.github.com>
2023-02-21 23:47:58 -0600

    added off-chain advances in addition contract

---
+++ off-chain/src/ScriptLoading.purs
@@ -0,0 +1,15 @@
+module ScriptLoading(loadScript,Path(Path)) where
+
+import Contract.Prelude (class Newtype)
+import Contract.Scripts (Validator)
+import Undefined (undefined)
+
+
+newtype Path = Path String
+
+derive instance Newtype Path _
+
+-- | load a script saved in a file 
+loadScript :: Path -> Validator
+loadScript = undefined
+
