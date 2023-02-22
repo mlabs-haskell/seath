f5c3ad188ed225a5ff0f7c37e055f337411bb01b
Luis-omega <73986926+Luis-omega@users.noreply.github.com>
2023-02-21 23:38:03 -0600

    first draft of AdditionValidator Done

--- on-chain/src/AdditionValidator.hs
+++ on-chain/src/AdditionValidator.hs
@@ -2,37 +2,38 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ViewPatterns #-}
 
-module AdditionValidator
-  ( additionScript,
-    mkAdditionValidator,
-    getAdditionValidator,
-  )
-where
+module AdditionValidator (
+  additionScript,
+  mkAdditionValidator,
+  getAdditionValidator,
+) where
 
 import Data.Eq.Deriving (deriveEq)
 import GHC.Generics (Generic)
-import Ledger (scriptHashAddress)
+import Ledger (Datum, scriptHashAddress)
 import Plutus.Script.Utils.V2.Typed.Scripts qualified as Scripts
 import Plutus.Script.Utils.V2.Typed.Scripts.Validators qualified as Validators
-import Plutus.V2.Ledger.Api
-  ( Address,
-    DatumHash,
-    Script,
-    ScriptContext (ScriptContext),
-    TxInfo (TxInfo, txInfoOutputs),
-    ValidatorHash,
-    fromCompiledCode,
-  )
-import Plutus.V2.Ledger.Tx (TxOut)
+import Plutus.V2.Ledger.Api (
+  Address,
+  Datum (Datum),
+  OutputDatum (OutputDatum),
+  Script,
+  ScriptContext (ScriptContext),
+  TxInfo (TxInfo, txInfoOutputs),
+  TxOut (TxOut),
+  ValidatorHash,
+  fromCompiledCode,
+  toBuiltinData,
+ )
 import PlutusTx qualified
-import PlutusTx.Prelude
-import PlutusTx.Trace qualified as PlutusTx
+import PlutusTx.Integer (Integer)
+import PlutusTx.Prelude (Bool (False), Maybe (Just, Nothing), (+), (.), (==))
+import PlutusTx.Trace qualified as Trace
 import Prelude qualified as Hask
 
 type AdditionParams = ()
 
--- FIXME: no instance for Natural
-data AdditionDatum = AdditionDatum {lockedAmount :: Hask.Integer}
+data AdditionDatum = AdditionDatum {lockedAmount :: Integer}
   deriving stock (Generic, Hask.Show)
 
 PlutusTx.makeLift ''AdditionDatum
@@ -42,8 +43,7 @@ PlutusTx.makeIsDataIndexed
 
 deriveEq ''AdditionDatum
 
--- FIXME: no instance for Natural
-data AdditionRedeemer = AdditionRedeemer {increaseAmount :: Hask.Integer}
+data AdditionRedeemer = AdditionRedeemer {increaseAmount :: Integer}
 
 PlutusTx.makeLift ''AdditionRedeemer
 PlutusTx.makeIsDataIndexed
@@ -62,25 +62,29 @@ mkAdditionValidator ::
 mkAdditionValidator _ datum redeemer context =
   case getOutputs context of
     [getDatumHash -> Just datHash] ->
-      PlutusTx.traceIfFalse
+      -- TODO : Check that the redeemer only contains positive values
+      Trace.traceIfFalse
         "Datum is updated incorrectly"
         (datumIsUpdated datum redeemer datHash)
-    _ -> trace "Only one Datum was expected" False
+    _ -> Trace.trace "Only one Datum was expected" False
 
 {-# INLINEABLE getOutputs #-}
 getOutputs :: ScriptContext -> [TxOut]
 getOutputs (ScriptContext TxInfo {txInfoOutputs} _) = txInfoOutputs
 
 {-# INLINEABLE datumIsUpdated #-}
-datumIsUpdated :: AdditionDatum -> AdditionRedeemer -> DatumHash -> Bool
+datumIsUpdated :: AdditionDatum -> AdditionRedeemer -> Datum -> Bool
+datumIsUpdated (AdditionDatum locked) (AdditionRedeemer toAdd) newDatum =
+  let expectedLockedValue = locked + toAdd
+      expectedDatum :: Datum
+      expectedDatum = (Datum . toBuiltinData . AdditionDatum) expectedLockedValue
+   in expectedDatum == newDatum
 
 {-# INLINEABLE getDatumHash #-}
-getDatumHash :: TxOut -> Maybe DatumHash
-getDatumHash = Hask.undefined -- TODO
-
--- | TODO : check that the datum corresponding to the hash has updated the value according to the needed value
--- bassically we want :  DatumLockValue + RedeeemrValue = DatumValueOfOutputTx
-datumIsUpdated = Hask.undefined
+-- we can use `import Plutus.V2.Ledger.Tx (outDatum)` instead
+getDatumHash :: TxOut -> Maybe Datum
+getDatumHash (TxOut _ _ (OutputDatum datum) _) = Just datum
+getDatumHash _ = Nothing
 
 data Addition
 
@@ -107,8 +111,8 @@ getAdditionValidator = Scripts.validatorScript . typedAdditionValidator
 getAdditionValidatorHash :: AdditionParams -> ValidatorHash
 getAdditionValidatorHash = Scripts.validatorHash . typedAdditionValidator
 
-getAdditionVAlidatorAddress :: AdditionParams -> Address
-getAdditionVAlidatorAddress = scriptHashAddress . getAdditionValidatorHash
+getAdditionValidatorAddress :: AdditionParams -> Address
+getAdditionValidatorAddress = scriptHashAddress . getAdditionValidatorHash
 
 additionScript :: Script
 additionScript = fromCompiledCode $$(PlutusTx.compile [||mkAdditionValidator||])
