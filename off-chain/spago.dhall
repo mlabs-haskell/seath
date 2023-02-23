febf837a98879e3ab863e899bf907c063a7ee033
Mikhail Lazarev <mikhail@mlabs.city>
2023-02-23 12:55:02 +0300

    small fixes after CTL update

--- off-chain/spago.dhall
+++ off-chain/spago.dhall
@@ -2,7 +2,7 @@
 Welcome to a Spago project!
 You can edit this file as you like.
 -}
-{ name = "ctl-package-example"
+{ name = "seath"
 , dependencies =
   [ "aff"
   , "bigints"
--- off-chain/webpack.config.js
+++ off-chain/webpack.config.js
@@ -90,7 +90,7 @@ module.exports = {
       debug: true,
     }),
     new HtmlWebpackPlugin({
-      title: "ctl-scaffold",
+      title: "seath",
       template: "./index.html",
       inject: false, // See stackoverflow.com/a/38292765/3067181
     }),
