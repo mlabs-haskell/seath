30671163914fc7b86c1afde3dd9e512f2adb9fd2
Luis-omega <73986926+Luis-omega@users.noreply.github.com>
2023-03-06 21:08:44 -0600

    added function to get ScriptInputs from a local transaction

--- off-chain/test/Examples/Utils.purs
+++ off-chain/test/Examples/Utils.purs
@@ -3,18 +3,25 @@ module Seath.Test.Examples.Utils
   , getScriptInputAndUtxos
   , getScriptUtxos
   , genPlutipWalletConfig
+  , genScriptUTXoFromTransaction
   ) where
 
 import Contract.Address (getNetworkId, validatorHashEnterpriseAddress)
+import Contract.Hashing as Ctl.Internal.Hashing
 import Contract.Log (logInfo')
 import Contract.Monad (Contract, liftContractM, liftedE)
 import Contract.PlutusData (class IsData)
+import Contract.Prelude (liftEffect, liftM, sequence)
 import Contract.ScriptLookups as ScriptLookups
 import Contract.Scripts (class ValidatorTypes, ValidatorHash)
 import Contract.Transaction
-  ( TransactionHash
+  ( FinalizedTransaction(FinalizedTransaction)
+  , TransactionHash
   , TransactionInput
+  , _body
   , _input
+  , _inputs
+  , _outputs
   , balanceTx
   , lookupTxHash
   , signTransaction
@@ -23,15 +30,22 @@ import Contract.Transaction
 import Contract.TxConstraints (TxConstraints)
 import Contract.Utxos (UtxoMap, utxosAt)
 import Control.Applicative (pure)
-import Control.Monad (bind)
-import Data.Array (head, replicate)
+import Control.Monad (bind, (>>=))
+import Ctl.Internal.Plutus.Conversion (toPlutusTxOutputWithRefScript)
+import Ctl.Internal.Serialization as Ctl.Internal.Serialization
+import Data.Array (filter, head, replicate)
 import Data.BigInt as BigInt
+import Data.Eq ((==))
 import Data.Functor ((<$>))
 import Data.Lens (view)
-import Data.Maybe (Maybe)
+import Data.Map as Map
+import Data.Maybe (Maybe, isJust)
 import Data.Monoid ((<>))
+import Data.Newtype (unwrap)
+import Data.Set (toUnfoldable)
 import Data.Show (show)
 import Data.Tuple.Nested (type (/\), (/\))
+import Effect.Aff (error)
 import Prelude (discard, ($))
 
 submitTxFromConstraintsWithLog
@@ -76,3 +90,33 @@ getScriptInputAndUtxos valHash txId = do
 
 genPlutipWalletConfig :: Int -> Array (Array BigInt.BigInt)
 genPlutipWalletConfig value = replicate value [ BigInt.fromInt 1_000_000_000 ]
+
+getFinalizedTransactionHash :: FinalizedTransaction -> Contract TransactionHash
+getFinalizedTransactionHash fTx = do
+  liftEffect $ Ctl.Internal.Hashing.transactionHash <$>
+    Ctl.Internal.Serialization.convertTransaction (unwrap fTx)
+
+genScriptUTXoFromTransaction
+  :: FinalizedTransaction -> ValidatorHash -> Contract UtxoMap
+genScriptUTXoFromTransaction fTx@(FinalizedTransaction tx) valHash = do
+  txId <- getFinalizedTransactionHash fTx
+  netId <- getNetworkId
+  validatorAddress <- liftContractM "cannot get validator address"
+    (validatorHashEnterpriseAddress netId valHash)
+  let
+    txInputs = view _inputs (view _body tx)
+    txOutputsCardano = view _outputs (view _body tx)
+  txInput <- liftM (error "Can't find Input in transaction") $ head
+    $ filter (\tx' -> (unwrap tx').transactionId == txId)
+    $ toUnfoldable txInputs
+  txOutputsPlutus <-
+    liftM (error "Can't find Outputs in transaction") $
+      sequence
+        ( filter
+            isJust
+            $ toPlutusTxOutputWithRefScript <$> txOutputsCardano
+        )
+  txOutputPlutus <- liftM (error "Can't find output in transaction") $ head $
+    filter (\tx' -> (unwrap (unwrap tx').output).address == validatorAddress)
+      txOutputsPlutus
+  pure $ Map.singleton txInput txOutputPlutus
