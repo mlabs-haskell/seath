71f6f9bc08937d28fd2ae4db324a7cacac25576e
Mikhail Lazarev <mikhail@mlabs.city>
2023-03-15 16:58:54 +0300

    wip: keys parsing and funding

--- off-chain/test/Setup/SetupMain.purs
+++ off-chain/test/Setup/SetupMain.purs
@@ -6,15 +6,17 @@ import Contract.Prelude
 import Data.Time.Duration
 import Undefined
 
-import Contract.Config (emptyHooks, testnetConfig)
+import Contract.Config (PrivatePaymentKeySource(..), PrivateStakeKeySource(..), WalletSpec(..), defaultOgmiosWsConfig, emptyHooks, mkCtlBackendParams, testnetConfig)
 import Contract.Monad (launchAff_, runContract, runContractInEnv, withContractEnv)
 import Contract.Test.Plutip (PlutipConfig)
-import Contract.Wallet (privateKeysToKeyWallet)
+import Contract.Wallet (PrivatePaymentKey(..), PrivateStakeKey(..), privateKeysToKeyWallet, withKeyWallet)
 import Contract.Wallet.Key (KeyWallet(..), keyWalletPrivatePaymentKey)
 import Contract.Wallet.KeyFile (privatePaymentKeyFromFile, privateStakeKeyFromFile)
 import Control.Monad.Error.Class (liftMaybe, try)
+import Data.Array (head)
 import Data.UInt (fromInt) as UInt
 import Effect.Class.Console (log)
+import Effect.Exception (error)
 import Node.FS.Aff (readdir)
 import Node.Path as Path
 import Seath.Test.Setup.ShareContract as Share
@@ -24,12 +26,13 @@ main = launchAff_ $ do
   (faucet /\ seathKeys) <-
     mkKeys "./test/Setup/keys/faucet" "./test/Setup/keys/seath_keys"
 
+  w0 <- liftMaybe (error "No wallet?") (head seathKeys)
+
   withContractEnv config $ \env -> do
-    runContractInEnv env (Share.payTo faucet faucet 500)
+    runContractInEnv env (withKeyWallet faucet $ Share.payTo w0 500)
 
   log "end"
 
--- runContract config $ Share.share mkShareConf
 
 -- mkShareConf :: Aff ShareConf
 mkKeys :: String -> String -> Aff (KeyWallet /\ Array KeyWallet)
@@ -44,7 +47,7 @@ mkKeys faucetPath targetKeys = do
 
   mekeSeathKeys = do
     keyDirs <- readdir targetKeys
-    for keyDirs $ \keyDir -> makeKeyWallet $ Path.concat [targetKeys, keyDir]
+    for keyDirs $ \keyDir -> makeKeyWallet $ Path.concat [ targetKeys, keyDir ]
 
   makeKeyWallet keysDir = do
     payment <- privatePaymentKeyFromFile $ Path.concat
@@ -56,3 +59,13 @@ mkKeys faucetPath targetKeys = do
     pure $ privateKeysToKeyWallet payment mbStake
 
 config = testnetConfig
+  { backendParams = mkCtlBackendParams
+      { ogmiosConfig: defaultOgmiosWsConfig
+      , kupoConfig:
+          { port: UInt.fromInt 1442
+          , host: "localhost"
+          , secure: false
+          , path: Nothing
+          }
+      }
+  }
--- off-chain/test/Setup/ShareContract.purs
+++ off-chain/test/Setup/ShareContract.purs
@@ -4,34 +4,41 @@ module Seath.Test.Setup.ShareContract
 
 import Contract.Prelude
 
-import Contract.Address (getNetworkId)
-import Contract.Chain (getTip)
+import Contract.Address (PaymentPubKeyHash, StakePubKeyHash, getNetworkId, ownPaymentPubKeyHash, ownStakePubKeyHash)
 import Contract.Log (logInfo')
-import Contract.Monad (Contract)
-import Contract.Wallet (getWalletUtxos, withKeyWallet)
+import Contract.Monad (Contract, liftedM)
+import Contract.ScriptLookups (ScriptLookups)
+import Contract.Transaction (awaitTxConfirmed, submitTxFromConstraints)
+import Contract.TxConstraints (mustPayToPubKeyAddress)
+import Contract.Value (Coin(..), Value, coinToValue)
+import Contract.Wallet (withKeyWallet)
 import Control.Monad (bind)
 import Ctl.Internal.Serialization.Address (addressBech32)
 import Ctl.Internal.Wallet.Key (KeyWallet)
+import Data.BigInt as BigInt
 import Data.Unit (Unit)
 
+payTo :: KeyWallet -> Int -> Contract Unit
+payTo toWallet adaAmount = do
+  netId <- getNetworkId
+  let addr = addressBech32 $ (unwrap toWallet).address netId
+  logInfo' $ "Funding " <> addr
 
-payTo :: KeyWallet -> KeyWallet -> Int -> Contract Unit
-payTo from to adaAmount = do
-  withKeyWallet from $ do
-    netId <- getNetworkId
-    logInfo' $ "NetId: " <> show netId
-    tip <- getTip
-    logInfo' $ "Tip: " <> show tip
-    let serializableAddr = (unwrap from).address netId
-    logInfo' $ "Faucet addr: " <> addressBech32 serializableAddr
-    utxos <- getWalletUtxos
-    logInfo' $ "Wallet UTXOs: " <> show utxos
+  (pkh /\ skh) <- getKeysHashes toWallet
+  let constraints = mustPayToPubKeyAddress pkh skh (adaValue adaAmount)
+  let lookups = mempty :: ScriptLookups Void
+  txId <- submitTxFromConstraints lookups constraints
+  awaitTxConfirmed txId
+  logInfo' "Contract end"
+
+getKeysHashes :: KeyWallet -> Contract (PaymentPubKeyHash /\ StakePubKeyHash)
+getKeysHashes kw = do
+  withKeyWallet kw $ do
+    pkh <- liftedM "Failed to get PubKeyHash" ownPaymentPubKeyHash
+    skh <- liftedM "Failed to get StakeKeyHash" ownStakePubKeyHash
     
-{-
-[INFO] 2023-03-15T11:01:13.821Z NetId: TestnetId
-[DEBUG] 2023-03-15T11:01:13.822Z sending: {"version":"1.0","type":"jsonwsp/request","servicename":"ogmios","mirror":"Query-3byu3qrlf9knoim","methodname":"Query","args":{"query":"chainTip"}}
-[DEBUG] 2023-03-15T11:01:13.825Z message: {"type":"jsonwsp/response","version":"1.0","servicename":"ogmios","methodname":"Query","result":{"slot":23194872,"hash":"4acd92f8ad1afeceb4243a3b9bbb1da20429995ffb230191cc82a98888298c5a"},"reflection":"Query-3byu3qrlf9knoim"}
-[INFO] 2023-03-15T11:01:13.827Z Tip: (Tip (ChainTip { blockHeaderHash: (BlockHeaderHash "4acd92f8ad1afeceb4243a3b9bbb1da20429995ffb230191cc82a98888298c5a"), slot: (Slot fromString "23194872") }))
-[INFO] 2023-03-15T11:01:13.828Z Faucet addr: addr_test1qzl8xfarvp90k605ppgpcqltfr62qahgd3pgy0jkal6zq5p8kp6hs4d6gdytf20gjqdfqzsl6vg04d5yfesyeeltyyfszsd4ph
-[INFO] 2023-03-15T11:01:13.837Z Wallet UTXOs: Nothing
--}
+    pure (pkh /\ skh)
+
+adaValue ∷ Int → Value
+adaValue ada = coinToValue
+  (Coin $ (BigInt.fromInt ada) * (BigInt.fromInt 1_000_000))
