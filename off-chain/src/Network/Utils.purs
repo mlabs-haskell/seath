b4ff17461bab37b4dce089230ef090ea5571c6bd
Luis-omega <73986926+Luis-omega@users.noreply.github.com>
2023-03-31 14:22:27 -0600

    added sane utils for uuid

--- off-chain/src/Network/Utils.purs
+++ off-chain/src/Network/Utils.purs
@@ -1,4 +1,4 @@
-module Seath.Network.Utils (getPublicKeyHash) where
+module Seath.Network.Utils (getPublicKeyHash,seathUUIDShow,seathParseUUID) where
 
 import Contract.Address (PubKeyHash, getWalletAddresses, toPubKeyHash)
 import Contract.Monad (Contract, liftedM)
@@ -7,6 +7,8 @@ import Control.Monad.Error.Class (liftMaybe)
 import Data.Array (head)
 import Data.Function (($))
 import Data.Functor ((<$>))
+import Data.Maybe (Maybe(Just,Nothing))
+import Data.UUID (UUID)
 import Effect.Aff (error)
 
 getPublicKeyHash :: Contract PubKeyHash
@@ -14,3 +16,12 @@ getPublicKeyHash = do
   address <- liftedM "can't get the address of KeyWallet" $ head <$>
     getWalletAddresses
   liftMaybe (error "can't get pubKeyHash of KeyWallet") $ toPubKeyHash address
+
+foreign import _seathUUIDShow :: UUID -> String
+foreign import _seathParseUUID ::  (forall x . x -> Maybe x) -> (forall x . Maybe x) ->String -> Maybe UUID
+
+seathUUIDShow :: UUID -> String
+seathUUIDShow = _seathUUIDShow
+
+seathParseUUID :: String -> Maybe UUID
+seathParseUUID = _seathParseUUID Just Nothing 
